bb05483341a8fa9beee6681e086c3bc195a83105
Matteo Piccini <gnappo.1@live.it>
2019-10-30 17:50:43 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -4,7 +4,13 @@ class Book
 
   def initialize(title)
     @title = title
+    save(brand)
   end
+  
+  def save(brand)
+    BRANDS << brand unless BRANDS.include?(brand)
+  end
+  
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
