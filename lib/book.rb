73aef62b03304fbb0c828694e6a009fd04ae5b2d
Matteo Piccini <gnappo.1@live.it>
2019-10-30 17:54:31 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,17 +1,19 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
   GENRES = []
   def initialize(title)
     @title = title
-    save(genre) if genre
   end
   
   def save(genre)
     GENRES << genre unless GENRES.include?(genre)
   end
   
-
+  def genre=(genre)
+    save(genre)  
+  end
+  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
